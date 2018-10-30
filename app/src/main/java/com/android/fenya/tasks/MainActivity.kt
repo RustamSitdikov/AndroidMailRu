2fc86257b76f6021ff737963feb4dd4df859570d
Rustam Sitdikov <sitdikov.rs@yandex.ru>
2018-10-30 09:27:00 +0300

    resolved bug with dublicate activity

--- app/src/main/java/com/android/fenya/tasks/MainActivity.kt
+++ app/src/main/java/com/android/fenya/tasks/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         const val ARGS_DELAY_TIME: String = "delay_time"
+        const val ARGS_TIMER_IS_RUNNING: String = "timer_is_running"
 
         const val MILLIS_PER_SECOND: Long = 1000
         const val SECONDS_TO_DELAY: Long = 2
@@ -18,50 +19,33 @@ class MainActivity : AppCompatActivity() {
 
     private val LOG_TAG: String = MainActivity::class.java::getName.toString()
 
-    private var mTimer: CountDownTimer? = null
+    private lateinit var mTimer: CountDownTimer
     private var mDelayTime: Long = SECONDS_TO_DELAY * MILLIS_PER_SECOND
+    private var mTimerIsRunning: Boolean = true
+
+    private fun startTimer(countDownMillis: Long) {
+        if (mTimerIsRunning) {
+            mTimer = object : CountDownTimer(countDownMillis, MILLIS_PER_SECOND) {
+                override fun onTick(millisUntilFinished: Long) {
+
+                }
+
+                override fun onFinish() {
+                    mTimerIsRunning = false
+                    startActivity(Intent(this@MainActivity, TimerActivity::class.java))
+                    finish()
+                }
+            }.start()
+        }
+    }
+
+    private fun stopTimer() {
+        mTimer.cancel()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onStart() {
-        super.onStart()
-
-        Log.i(LOG_TAG, "onStart")
-        startTimer(mDelayTime)
-    }
-
-    private fun startTimer(countDownMillis: Long) {
-        mTimer = object : CountDownTimer(countDownMillis, MILLIS_PER_SECOND) {
-            override fun onTick(millisUntilFinished: Long) {
-
-            }
-
-            override fun onFinish() {
-                startActivity(Intent(this@MainActivity, TimerActivity::class.java))
-                finish()
-            }
-        }.start()
-    }
-
-    private fun stopTimer() {
-        mTimer?.cancel()
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        Log.i(LOG_TAG, "onStop")
-        stopTimer()
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        Log.i(LOG_TAG, "onPause")
-        stopTimer()
     }
 
     override fun onResume() {
@@ -71,17 +55,26 @@ class MainActivity : AppCompatActivity() {
         startTimer(mDelayTime)
     }
 
+    override fun onPause() {
+        super.onPause()
+
+        Log.i(LOG_TAG, "onPause")
+        stopTimer()
+    }
+
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
         Log.i(LOG_TAG, "onRestoreInstanceState")
         mDelayTime = savedInstanceState.getLong(ARGS_DELAY_TIME)
+        mTimerIsRunning = savedInstanceState.getBoolean(ARGS_TIMER_IS_RUNNING)
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         Log.i(LOG_TAG, "onSaveInstanceState")
         outState?.run {
             putLong(ARGS_DELAY_TIME, mDelayTime)
+            putBoolean(ARGS_TIMER_IS_RUNNING, mTimerIsRunning)
         }
 
         super.onSaveInstanceState(outState)
--- app/src/main/java/com/android/fenya/tasks/TimerActivity.kt
+++ app/src/main/java/com/android/fenya/tasks/TimerActivity.kt
@@ -13,7 +13,7 @@ class TimerActivity : AppCompatActivity() {
     companion object {
         const val ARGS_COUNTING_TIME: String = "countintg_time"
         const val ARGS_TIMER_TIME: String = "timer_time"
-        const val ARGS_IS_RUNNING: String = "is_running"
+        const val ARGS_TIMER_IS_RUNNING: String = "timer_is_running"
 
         const val MILLIS_PER_SECOND: Long = 1000
         const val SECONDS_TO_COUNTDOWN: Long = 20
@@ -30,23 +30,12 @@ class TimerActivity : AppCompatActivity() {
     lateinit var mTextView: TextView
 
     private var mTimer: CountDownTimer? = null
-    private var mCountingTime: Long = 0
-    private var mIsRunning: Boolean = false
+    private var mCountingTime: Long = SECONDS_TO_COUNTDOWN * MILLIS_PER_SECOND
+    private var mTimerIsRunning: Boolean = false
 
-    lateinit var mUnits: Array<String>
-    lateinit var mTens: Array<String>
-    lateinit var mHundreds: Array<String>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer)
-
-        mButton = findViewById(R.id.timerButton)
-        mTextView = findViewById(R.id.timerTextView)
-
-        initResources()
-        initValues()
-    }
+    private lateinit var mUnits: Array<String>
+    private lateinit var mTens: Array<String>
+    private lateinit var mHundreds: Array<String>
 
     private fun initResources() {
         mUnits = resources.getStringArray(R.array.units)
@@ -56,60 +45,68 @@ class TimerActivity : AppCompatActivity() {
 
     private fun initValues() {
         mCountingTime = SECONDS_TO_COUNTDOWN * MILLIS_PER_SECOND
-        mIsRunning = false
+        mTimerIsRunning = false
         mButton.setText(R.string.start)
+        mTextView.setText(R.string.nothing)
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun startTimer(countDownMillis: Long) {
+        if (mTimerIsRunning) {
+            mButton.setText(R.string.stop)
+            mTimer = object : CountDownTimer(countDownMillis, MILLIS_PER_SECOND) {
+                override fun onTick(millisUntilFinished: Long) {
+                    mCountingTime = millisUntilFinished
+                    mTextView.text = num2str((SECONDS_TO_COUNTDOWN - mCountingTime / MILLIS_PER_SECOND).toInt())
+                }
+
+                override fun onFinish() {
+                    initValues()
+                }
+            }.start()
+        }
+    }
+
+    private fun stopTimer() {
+        mButton.setText(R.string.start)
+        mTimer?.cancel()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.i(LOG_TAG, "onCreate")
+        setContentView(R.layout.activity_timer)
+
+        mButton = findViewById(R.id.timerButton)
+        mTextView = findViewById(R.id.timerTextView)
+
+        initValues()
+        initResources()
 
         mButton.setOnClickListener {
-            if (!mIsRunning) {
+            if (!mTimerIsRunning) {
+                mTimerIsRunning = true
                 startTimer(mCountingTime)
             } else {
+                mTimerIsRunning = false
                 stopTimer()
                 initValues()
             }
         }
     }
 
-    private fun startTimer(countDownMillis: Long) {
-        mButton.setText(R.string.stop)
+    override fun onResume() {
+        super.onResume()
 
-        mTimer = object : CountDownTimer(countDownMillis, MILLIS_PER_SECOND) {
-            override fun onTick(millisUntilFinished: Long) {
-                mCountingTime = millisUntilFinished
-                mTextView.text = num2str((SECONDS_TO_COUNTDOWN - mCountingTime / MILLIS_PER_SECOND).toInt())
-            }
-
-            override fun onFinish() {
-                initValues()
-            }
-        }.start()
-
-        mIsRunning = true
-    }
-
-    private fun stopTimer() {
-        mTimer?.cancel()
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        stopTimer()
+        Log.i(LOG_TAG, "onResume")
+        startTimer(mCountingTime)
     }
 
     override fun onPause() {
         super.onPause()
 
+        Log.i(LOG_TAG, "onPause")
         stopTimer()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        if (mIsRunning) startTimer(mCountingTime)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
@@ -118,7 +115,7 @@ class TimerActivity : AppCompatActivity() {
         Log.i(LOG_TAG, "onRestoreInstanceState")
         mCountingTime = savedInstanceState.getLong(ARGS_COUNTING_TIME)
         mTextView.text = savedInstanceState.getString(ARGS_TIMER_TIME)
-        mIsRunning = savedInstanceState.getBoolean(ARGS_IS_RUNNING)
+        mTimerIsRunning = savedInstanceState.getBoolean(ARGS_TIMER_IS_RUNNING)
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
@@ -126,7 +123,7 @@ class TimerActivity : AppCompatActivity() {
         outState?.run {
             putLong(ARGS_COUNTING_TIME, mCountingTime)
             putString(ARGS_TIMER_TIME, mTextView.toString())
-            putBoolean(ARGS_IS_RUNNING, mIsRunning)
+            putBoolean(ARGS_TIMER_IS_RUNNING, mTimerIsRunning)
         }
 
         super.onSaveInstanceState(outState)
